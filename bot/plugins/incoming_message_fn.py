47db0998856a62ef7d7d24ae6c765eff2663afe2
Munalia <69430348+Munalia@users.noreply.github.com>
2021-02-22 13:21:32 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -60,7 +60,7 @@ async def incoming_start_message_f(bot, update):
             if user.status == "kicked":
                await bot.send_message(
                    chat_id=update.chat.id,
-                   text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                   text="Sorry Sir, You are Banned to use me. Contact my [Owner](https://t.me/KumarSwamiKS).",
                    parse_mode="markdown",
                    disable_web_page_preview=True
                )
